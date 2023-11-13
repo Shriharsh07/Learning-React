e6eead6174b626c993c36b90d3e60d902e3e6ed8
Shriharsh07 <shriharshdpattar@gmail.com>
2023-11-13 15:39:37 +0530

    ReactDay6ContextAPI

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>React with chai Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
