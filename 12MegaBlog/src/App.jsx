1d809abe5ad3d91fac676c801abc6189b2eaf4ac
Shriharsh07 <shriharshdpattar@gmail.com>
2023-11-17 17:19:27 +0530

    Blog day 1

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <div>
+        <h1>Blog App </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
