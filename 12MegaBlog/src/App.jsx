7297806ea9535cad508f703c4285cc41df2202dd
Shriharsh07 <shriharshdpattar@gmail.com>
2023-11-20 11:13:32 +0530

    MegaBlog

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,38 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-      <div>
-        <h1>Blog App </h1>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          Hello <Outlet />
+        </main>
+        <Footer />
       </div>
-    </>
-  )
+    </div>
+  ) : null
 }
 
 export default App
